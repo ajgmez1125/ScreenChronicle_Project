192e8bb285be0428e4468b77f8f0090626de8cc3
Ayden <ajgg1125@gmail.com>
2024-04-02 21:45:43 -0400

    ui started

--- Infrastructure/src/main/java/org/humber/project/controllers/MVCController.java
+++ Infrastructure/src/main/java/org/humber/project/controllers/MVCController.java
@@ -5,29 +5,50 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.List;
-
 import org.humber.project.domain.Movie;
+import org.humber.project.domain.Review;
 
 @Controller
-public class MVCController
-{
-    private RestTemplate restTemplate;
+public class MVCController {
 
-    public MVCController(RestTemplateBuilder restTemplate)
-    {
-        this.restTemplate = restTemplate.build();
+    private final RestTemplate restTemplate;
+
+    public MVCController(RestTemplateBuilder restTemplateBuilder) {
+        this.restTemplate = restTemplateBuilder.build();
     }
 
-    @GetMapping()
-    public String homepage(Model model)
-    {
-        ResponseEntity<Movie[]> moviesResponse = restTemplate.getForEntity("http://localhost:8000/rest/movie", Movie[].class);
+    @GetMapping("/")
+    public String homepage(Model model) {
+        ResponseEntity<Movie[]> moviesResponse = restTemplate.getForEntity("http://localhost:8080/api/movie", Movie[].class);
         Movie[] movies = moviesResponse.getBody();
         model.addAttribute("movies", movies);
-        //System.out.println(model.toString());
         return "index";
     }
+
+    @GetMapping("/movie/{movie_id}")
+    public String movie(Model model, @PathVariable Long movie_id) {
+        ResponseEntity<Movie> movieResponse = restTemplate.getForEntity("http://localhost:8080/api/movie/"+movie_id, Movie.class);
+        Movie movie = movieResponse.getBody();
+        model.addAttribute("movie", movie);
+
+        ResponseEntity<Review[]> reviewResponse = restTemplate.getForEntity("http://localhost:8080/api/review/movie/"+movie_id, Review[].class);
+        Review[] reviews = reviewResponse.getBody();
+        model.addAttribute("reviews", reviews);
+        return "movie";
+    }
+
+    @GetMapping("/add-movie")
+    public String addMovie(Model model)
+    {
+        return "add-movie";
+    }
+
+    // @GetMapping("/recommended/{user_id}")
+    // public String recommendedMovies(Model model)
+    // {
+    //     return "recommended-movies";
+    // }
 }
